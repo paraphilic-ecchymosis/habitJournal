d8acaee0f4de00d7e5203a7abddffaff53c490da
Jacob Crook <jakecrook@live.com>
2020-07-02 11:11:42 -0600

    Started thymeleaf wiring and reworking auth.

--- src/main/java/com/crookedcoder/behaviorapp/config/Config.java
+++ src/main/java/com/crookedcoder/behaviorapp/config/Config.java
@@ -1,16 +1,83 @@
 package com.crookedcoder.behaviorapp.config;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 import lombok.Data;
 
 @Configuration
-public @Data class Config {
+public class Config {
 
-    @Autowired
+    // @Bean
     @Value("${application.name}")
-    private String applicationName;
+    public String applicationName;
 
+        /* GSuite Config */
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
+    @Bean
+    GoogleClientSecrets clientSecrets() throws IOException {
+        Resource credentials = new ClassPathResource("credentials.json");
+        return GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(credentials.getInputStream()));
+    }
+
+    /* Thymeleaf Config */
+
+    @Bean
+    @Description("Thymeleaf Template Resolver")
+    public ServletContextTemplateResolver templateResolver() {
+        // TODO: This needs to be inspected for accuracy.
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(null);
+        templateResolver.setPrefix("/WEB-INF/views/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+
+        return templateResolver;
+    }
+
+    @Bean
+    @Description("Thymeleaf Template Engine")
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setTemplateEngineMessageSource(messageSource());
+        return templateEngine;
+    }
+
+    
+    @Bean
+    @Description("Thymeleaf View Resolver")
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setOrder(1);
+        return viewResolver;
+    }
+    
+    // TODO: Need message properties
+    @Bean
+    @Description("Spring Message Resolver")
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+    
 }
