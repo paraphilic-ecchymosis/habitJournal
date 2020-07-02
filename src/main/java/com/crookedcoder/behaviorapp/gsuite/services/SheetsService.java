0e330f45b60a4570ededa1722812acb4587030be
Jacob Crook <jakecrook@live.com>
2020-07-02 11:13:19 -0600

    More changes for auth.

--- src/main/java/com/crookedcoder/behaviorapp/gsuite/services/SheetsService.java
+++ src/main/java/com/crookedcoder/behaviorapp/gsuite/services/SheetsService.java
@@ -1,25 +1,29 @@
 package com.crookedcoder.behaviorapp.gsuite.services;
 
-import java.io.IOException;
 import java.security.GeneralSecurityException;
 
-import com.crookedcoder.behaviorapp.gsuite.GSuiteAuthorizeUtil;
+import com.crookedcoder.behaviorapp.gsuite.GSuiteAuthorizeServiceImpl;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.sheets.v4.Sheets;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SheetsService {
 
-    private static String APPLICATION_NAME = "Behavior App";
+    @Autowired
+    private String APPLICATION_NAME;
 
-    public Sheets getSheetsService() throws IOException, GeneralSecurityException {
+
+    GSuiteAuthorizeServiceImpl gSuiteAuthorizeServiceImpl;
+
+    public Sheets getSheetsService() throws Exception, GeneralSecurityException {
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        Credential credential = GSuiteAuthorizeUtil.authorizeUser(HTTP_TRANSPORT);
+        Credential credential = gSuiteAuthorizeServiceImpl.authorizeUser(HTTP_TRANSPORT);
         return new Sheets.Builder(
           GoogleNetHttpTransport.newTrustedTransport(), 
           JacksonFactory.getDefaultInstance(), credential)
