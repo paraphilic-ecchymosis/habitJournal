4cacfe7d2ed82e634063e7c1d944d7ac08ade4c3
Jacob Crook <jakecrook@live.com>
2020-07-04 20:05:22 -0600

    More thymeleaf/frontend stuff, plus stub for sec.

--- src/main/java/com/crookedcoder/persistence/security/SecurityConfiguration.java
+++ src/main/java/com/crookedcoder/persistence/security/SecurityConfiguration.java
@@ -1,21 +1,16 @@
 package com.crookedcoder.persistence.security;
 
-import java.net.ProtocolException;
-import java.util.Properties;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled=true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        
 
     }
 }
