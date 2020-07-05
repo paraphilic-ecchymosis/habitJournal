f846d24d8613816c560f7192e6c54cd76ec88d5d
Jacob Crook <jakecrook@live.com>
2020-07-05 02:03:03 -0600

    More security config, this is intentional crap.

--- src/main/java/com/crookedcoder/persistence/config/ViewConfig.java
+++ src/main/java/com/crookedcoder/persistence/config/ViewConfig.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -24,19 +26,17 @@ public class ViewConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/files/**")
-                .addResourceLocations("/WEB-INF/pdf/");
+        registry.addResourceHandler("/files/**").addResourceLocations("/WEB-INF/pdf/");
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
     }
-    
+
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
-        SpringResourceTemplateResolver templateResolver = new
-            SpringResourceTemplateResolver();
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
         templateResolver.setApplicationContext(applicationContext);
         templateResolver.setPrefix("/WEB-INF/views/");
         templateResolver.setSuffix(".html");
--- src/main/java/com/crookedcoder/persistence/security/SecurityConfiguration.java
+++ src/main/java/com/crookedcoder/persistence/security/SecurityConfiguration.java
@@ -1,16 +1,23 @@
 package com.crookedcoder.persistence.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Order(2)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http
+            .authorizeRequests()
+            .anyRequest()
+            .authenticated()
+                .and()
+            .httpBasic()
+                .and()
+            .logout();
     }
 }
